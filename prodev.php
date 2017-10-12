e7c3d32a71b0cb7b249a2ab26c598d8428027829
Michael Romero <m.romero1989@gmail.com>
2017-10-11 20:49:11 -0600

    Added headings and paragraphs and will fill them out later.

--- prodev.php
+++ prodev.php
@@ -7,63 +7,50 @@
 	</head>
 	<body>
 		<!-- <img class="displayed" src="images/minimal.jpg" alt=""/> -->
-		<h1>Michael Romero's Profesional Development</h1>
-		<h2>My Links</h2>
+		<h1>MICHAEL ROMERO'S PROFESSIONAL DEVELOPMENT</h1>
+		<SCRIPT class="pageupdate" LANGUAGE="JavaScript">
+			update = new Date(document.lastModified);
+			theMonth = update.getMonth() + 1;
+			theDate = update.getDate();
+			theYear = update.getFullYear();
+			document.writeln("<em>Last updated:" + theMonth + "/" + theDate + "/" + theYear + "</em>");
+		</SCRIPT>
+		<h2>LINKS</h2>
 		<ul>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
+			<li>Null</li>
 		</ul>
+		<h2>HOMEWORK ACTIVITIES</h2>
+		<h3>MY LEARNING STYLE</h3>
+		<h4>VISUAL</h4>
+		<ul>
+			<li>20% Auditory</li>
+			<li>50% Visual</li>
+			<li>30% Tactile</li>
+		</ul>
+		<h3>Quantitative Accomplishments</h3>
+		<ul>
+			<li>Thing 1</li>
+			<li>Thing 2</li>
+			<li>Thing 3</li>
+		</ul>
+		<h3>Qualitative Accomplishments</h3>
+		<ul>
+			<li>Thing 1</li>
+			<li>Thing 2</li>
+			<li>Thing 3</li>
+		</ul>
+		<h3>Golden Circle</h3>
+		<ul>
+			<li>Thing 1</li>
+			<li>Thing 2</li>
+			<li>Thing 3</li>
+		</ul>
+		<h3>Elevator Pitch</h3>
+		<h5>Version 1</h5>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam laoreet viverra metus vitae scelerisque. Nunc varius augue sem, tincidunt pellentesque est ultricies a. Aliquam id.</p>
+		<h5>Version 2</h5>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam laoreet viverra metus vitae scelerisque. Nunc varius augue sem, tincidunt pellentesque est ultricies a. Aliquam id.</p>
+		<h2>WEEKLY REFLECTIONS</h2>
+
 	</body>
 </html>
