323dd7ffe31e6724be3f264ebf8856c48dcdee4d
Michael Romero <m.romero1989@gmail.com>
2017-10-16 06:31:59 -0600

    Added golden circle info + elevator pitch.

--- prodev.php
+++ prodev.php
@@ -1,14 +1,14 @@
-<!DOCTYPE>
+<!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="utf-8"/>
 		<title>Michael Romero's Profesional Development</title>
-		<link href="style.css" type="text/css" rel="stylesheet">
+		<link href="style.css" type="text/css" rel="stylesheet"/>
 	</head>
 	<body>
 		<h1>MICHAEL ROMERO'S PROFESSIONAL DEVELOPMENT</h1>
 		<!-- This creates a page update to show user when it was modified-->
-		<SCRIPT class="pageupdate" LANGUAGE="JavaScript">
+		<SCRIPT>
 			update = new Date(document.lastModified);
 			theMonth = update.getMonth() + 1;
 			theDate = update.getDate();
@@ -29,31 +29,31 @@
 		</ul>
 		<h3>Quantitative Accomplishments</h3>
 		<ul>
-			<li>Thing 1</li>
-			<li>Thing 2</li>
+			<li>Scheduled + or - 300 patients to receive cancer ultrasounds at UNMH.</li>
+			<li>Trained 20-30 new employees</li>
 			<li>Thing 3</li>
 		</ul>
 		<h3>Qualitative Accomplishments</h3>
 		<ul>
-			<li>Thing 1</li>
-			<li>Thing 2</li>
+			<li>Learned the systems programs at work faster than anyone else.</li>
+			<li>Finished the queue of clinics patient lists fast which lead to better team workload.</li>
 			<li>Thing 3</li>
 		</ul>
 		<h3>Golden Circle</h3>
 		<ul>
-			<li>Thing 1</li>
-			<li>Thing 2</li>
-			<li>Thing 3</li>
+			<li>Why? Life is Short.</li>
+			<li>How? By learning the fundamentals of code.</li>
+			<li>What? Code, learn, have fun, start a business, makes apps, etc.</li>
 		</ul>
 		<h3>Elevator Pitch</h3>
 		<h5>Version 1</h5>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam laoreet viverra metus vitae scelerisque. Nunc varius augue sem, tincidunt pellentesque est ultricies a. Aliquam id.</p>
+		<p>Hello, my name is Michael. I own a business which is collaboration of artists that work to make peoples passions more attainable. I did this by learning how to code. Would you like to know more about this business?</p>
 		<h5>Version 2</h5>
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam laoreet viverra metus vitae scelerisque. Nunc varius augue sem, tincidunt pellentesque est ultricies a. Aliquam id.</p>
 		<h2>WEEKLY REFLECTIONS</h2>
 
 	</body>
+	<!--footer is left empty because in css it creates an empty space on the web page-->
 	<footer>
-
 	</footer>
 </html>
