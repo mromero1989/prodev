2800fcf9245af0a14237451c7bcfc200270c2fe6
Michael Romero <m.romero1989@gmail.com>
2017-10-29 14:53:41 -0600

    cleaned it up

--- prodev.php
+++ prodev.php
@@ -2,59 +2,66 @@
 <html>
 	<head>
 		<meta charset="utf-8"/>
-		<title>Michael Romero's Profesional Development</title>
 		<link href="style.css" type="text/css" rel="stylesheet"/>
+		<title>Michael Romero's Profesional Development</title>
 	</head>
+
 	<body>
-		<h1>MICHAEL ROMERO'S PROFESSIONAL DEVELOPMENT</h1>
-		<!-- This creates a page update to show user when it was modified-->
-		<SCRIPT>
-			update = new Date(document.lastModified);
-			theMonth = update.getMonth() + 1;
-			theDate = update.getDate();
-			theYear = update.getFullYear();
-			document.writeln("<em>Last updated:" + theMonth + "/" + theDate + "/" + theYear + "</em>");
-		</SCRIPT>
-		<h3>LINKS</h3>
-		<ul>
-			<li>Null</li>
-		</ul>
-		<h2>HOMEWORK ACTIVITIES</h2>
-		<h3>MY LEARNING STYLE</h3>
-		<h4>VISUAL</h4>
-		<ul>
-			<li>20% Auditory</li>
-			<li>50% Visual</li>
-			<li>30% Tactile</li>
-		</ul>
-		<h3>Quantitative Accomplishments</h3>
-		<ul>
-			<li>Scheduled + or - 300 patients to receive cancer ultrasounds at UNMH.</li>
-			<li>Trained 20-30 new employees</li>
-			<li>Thing 3</li>
-		</ul>
-		<h3>Qualitative Accomplishments</h3>
-		<ul>
-			<li>Learned the systems programs at work faster than anyone else.</li>
-			<li>Finished the queue of clinics patient lists fast which lead to a better team workload.</li>
-			<li>Thing 3</li>
-		</ul>
-		<h3>Golden Circle</h3>
-		<ul>
-			<li>Why? Life is Short.</li>
-			<li>How? By learning the fundamentals of code.</li>
-			<li>What? Code, learn, have fun, start a business, makes apps, etc.</li>
-		</ul>
-		<h3>Elevator Pitch</h3>
-		<h5>Version 1</h5>
-		<p>Hello, my name is Michael. I own a business which is collaboration of artists that work to make peoples passions more attainable. I did this by learning how to code. Would you like to know more about this business?</p>
-		<h5>Version 2</h5>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam laoreet viverra metus vitae scelerisque. Nunc varius augue sem, tincidunt pellentesque est ultricies a. Aliquam id.</p>
-		<h2>WEEKLY REFLECTIONS</h2>
-		<h3>Week 0</h3>
-		<p>Overall week 0 has been fascinating and I'm slowly finding coming here routine. It was somewhat frightening coming to class because I wasn't sure what to expect. Although it has been hard it is also fun. My only problem is not getting simple instructions because I tend to over think or not fully recognize what I need to do. Although I might take longer than some to learn I feel like dedication and perseverance will get me though this bootcamp. So far, I'm feeling good.</p>
-		<h3>Week 1</h3>
-		<p>Week 1 started on a good note and moved into uncharted territory. My number one problem is getting tired around 3. My mind stops working and I stop listening to the lectures. I get home and I'm tired which prevents me from taking in and understanding the material. I am also having fun and can't wait to understand everything. It's my wanting to learn that keeps me driven. I imagine the life I am going to have after this bootcamp and I want to see myself go far in life. It's hard and fun at the same time. I see a lot of people in the same position and it makes me feel less alone. I can't wait to actively work on group projects and see the results of our hard work. I really wish I had a tutor so I can understand the concepts more. I am slightly behind but I know I have to work harder if I want to succeed. </p>
+		<header>
+			<h1>MICHAEL ROMERO'S PROFESSIONAL DEVELOPMENT</h1>
+			<!-- This creates a page update to show user when it was modified-->
+			<SCRIPT>
+				update = new Date(document.lastModified);
+				theMonth = update.getMonth() + 1;
+				theDate = update.getDate();
+				theYear = update.getFullYear();
+				document.writeln("<em>Last updated:" + theMonth + "/" + theDate + "/" + theYear + "</em>");
+			</SCRIPT>
+			<h3>LINKS</h3>
+			<ul>
+				<li>Null</li>
+			</ul>
+		</header>
+
+		<main>
+			<h2>HOMEWORK ACTIVITIES</h2>
+			<h3>MY LEARNING STYLE</h3>
+			<h4>VISUAL</h4>
+			<ul>
+				<li>20% Auditory</li>
+				<li>50% Visual</li>
+				<li>30% Tactile</li>
+			</ul>
+			<h3>Quantitative Accomplishments</h3>
+			<ul>
+				<li>Scheduled + or - 300 patients to receive cancer ultrasounds at UNMH.</li>
+				<li>Trained 20-30 new employees</li>
+				<li>Thing 3</li>
+			</ul>
+			<h3>Qualitative Accomplishments</h3>
+			<ul>
+				<li>Learned the systems programs at work faster than anyone else.</li>
+				<li>Finished the queue of clinics patient lists fast which lead to a better team workload.</li>
+				<li>Thing 3</li>
+			</ul>
+			<h3>Golden Circle</h3>
+			<ul>
+				<li>Why? Life is Short.</li>
+				<li>How? By learning the fundamentals of code.</li>
+				<li>What? Code, learn, have fun, start a business, makes apps, etc.</li>
+			</ul>
+			<h3>Elevator Pitch</h3>
+			<h5>Version 1</h5>
+			<p>Hello, my name is Michael. I own a business which is collaboration of artists that work to make peoples passions more attainable. I did this by learning how to code. Would you like to know more about this business?</p>
+			<h5>Version 2</h5>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam laoreet viverra metus vitae scelerisque. Nunc varius augue sem, tincidunt pellentesque est ultricies a. Aliquam id.</p>
+			<h2>WEEKLY REFLECTIONS</h2>
+			<h3>Week 0</h3>
+			<p>Overall week 0 has been fascinating and I'm slowly finding coming here routine. It was somewhat frightening coming to class because I wasn't sure what to expect. Although it has been hard it is also fun. My only problem is not getting simple instructions because I tend to over think or not fully recognize what I need to do. Although I might take longer than some to learn I feel like dedication and perseverance will get me though this bootcamp. So far, I'm feeling good.</p>
+			<h3>Week 1</h3>
+			<p>Week 1 started on a good note and moved into uncharted territory. My number one problem is getting tired around 3. My mind stops working and I stop listening to the lectures. I get home and I'm tired which prevents me from taking in and understanding the material. I am also having fun and can't wait to understand everything. It's my wanting to learn that keeps me driven. I imagine the life I am going to have after this bootcamp and I want to see myself go far in life. It's hard and fun at the same time. I see a lot of people in the same position and it makes me feel less alone. I can't wait to actively work on group projects and see the results of our hard work. I really wish I had a tutor so I can understand the concepts more. I am slightly behind but I know I have to work harder if I want to succeed. </p>
+		</main>
+
 	</body>
 	<!--footer is left empty because in css it creates an empty space on the web page-->
 	<footer>
